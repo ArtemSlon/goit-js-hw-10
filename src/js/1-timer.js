d22c7f2f532f67ee50c0cf7b139c03227eae1f30
Artem Slonchynskyi <slonartem24@gmail.com>
2025-11-19 00:45:00 +0200

    done scripts css styles work in process

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,4 +1,3 @@
-
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import iziToast from "izitoast";
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,32 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+const form = document.querySelector(".form");
+form.addEventListener("submit", onFormSubmit);
+function onFormSubmit (event) {
+    event.preventDefault();
+    const delay = Number(event.target.elements.delay.value)
+    const state = event.target.elements.state.value;
+    const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (state === "fulfilled") {
+      resolve(delay);
+    } else {
+      reject(delay);
+    }
+  }, delay);
+    });
+    promise
+  .then(ms => {
+    iziToast.success({
+      message: `✅ Fulfilled promise in ${ms}ms`,
+      position: "topRight",
+    });
+  })
+  .catch(ms => {
+    iziToast.error({
+      message: `❌ Rejected promise in ${ms}ms`,
+      position: "topRight",
+    });
+  });
+}
+
